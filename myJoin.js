71d0542db716a26d8c283daf24de14892305eabc
Pruthvi Raj <pruthviraj.1rn16ec097@gmail.com>
2024-10-24 08:21:56 +0530

    added: Join function

---
+++ myJoin.js
@@ -0,0 +1,13 @@
+Array.prototype.myJoin = function (separator = ",") {
+    let result = "";
+  
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        result += this[i];
+      } else {
+        result += this[i] + separator;
+      }
+    }
+  
+    return result;
+};
